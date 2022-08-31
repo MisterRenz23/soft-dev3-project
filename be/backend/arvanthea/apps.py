6be953c3d76197aa3b54c659bcf12f4e72e613f2
EN Rex George <enrexgeorge.verdida@sun-asterisk.com>
2022-08-31 10:57:05 +0800

    [BE] Initial Commit

--- be/backend/arvanthea/apps.py
+++ be/backend/arvanthea/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HealthdevConfig(AppConfig):
+class ArvantheaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'healthdev'
+    name = 'arvanthea'
