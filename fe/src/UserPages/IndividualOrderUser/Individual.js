ba4aba4e8b1799f627fc5342763c3b668bd80c59
unknown <johnrenz.valencia@cit.edu>
2022-09-27 10:27:57 +0800

    [FE] Improve UI - Individual Tray

--- fe/src/UserPages/IndividualOrderUser/Individual.js
+++ fe/src/UserPages/IndividualOrderUser/Individual.js
@@ -1,39 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Container, Image, Row, Col, Card, Button } from 'react-bootstrap';
-import API from '../../API';
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Container, Image, Row, Col, Card, Button } from "react-bootstrap";
+import API from "../../API";
+import "./Individual.css";
 const Individual = () => {
-    const [products, setProducts] = useState([])
-    const getProducts = async () => {
-        const response = await API.get('user/product_list/')
-        setProducts(response.data)
-    }
+  const [products, setProducts] = useState([]);
+  const getProducts = async () => {
+    const response = await API.get("user/product_list/");
+    setProducts(response.data);
+  };
 
-    useEffect(() => {
-        getProducts();
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-    }, []);
+  return (
+    <div>
+      <Container>
+        <Row>
+          {products.map((product, index) => (
+            <Col>
+              <Card style={{ width: "30rem" }}>
+                <Card.Img variant="top" src={product.image} />
+                <Card.Body>
+                  <Card.Title> {product.title} </Card.Title>
+                  <Card.Text>{product.description}</Card.Text>
+                  <Link to="/services-user/individual-order/1">
+                    <Button variant="primary">Order Now</Button>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            {
-                products.map((product, index) => (
-                    <Card style={{ width: '30rem' }}>
-                        <Card.Img variant="top" src={product.image} />
-                        <Card.Body>
-                            <Card.Title> {product.title} </Card.Title>
-                            <Card.Text>{product.description}</Card.Text>
-                            <Link to={`/services-user/individual-order/${product.id}`}>
-                                <Button variant="primary">Order Now</Button>
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                )
-                )
-            }
-        </div>
-    );
-}
-
-export default Individual
+export default Individual;
