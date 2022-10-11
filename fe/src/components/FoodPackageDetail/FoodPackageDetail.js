942d899e34606e8f0acc49f5be9d2743f68becb7
EN Rex George <111893461+ENRexGeorge23@users.noreply.github.com>
2022-10-11 10:27:28 +0800

    Update FoodPackageDetail.js

--- fe/src/components/FoodPackageDetail/FoodPackageDetail.js
+++ fe/src/components/FoodPackageDetail/FoodPackageDetail.js
@@ -62,7 +62,7 @@ const FoodPackageDetail = () => {
               Good for 10 - 15 persons
             </p>
 
-            <p className={styles['product-description']}>{set.description}</p>
+            <p className={styles['product-description']}>{sets.description}</p>
 
             <p>For customization, messages us after you have check this out.</p>
             <div className={styles['counter']}>
